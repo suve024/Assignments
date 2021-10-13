efa2066c45bb6fb99297261770f8abf8d7ca06c0
Suvetha S <81619407+suve024@users.noreply.github.com>
2021-10-13 18:59:03 +0530

    Update AcmeLogin.java

--- week2/day2/AcmeLogin.java
+++ week2/day2/AcmeLogin.java
@@ -13,8 +13,8 @@ public class AcmeLogin {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.get("https://acme-test.uipath.com/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.findElement(By.id("email")).sendKeys("kumar.testleaf@gmail.com");
 		driver.findElement(By.id("password")).sendKeys("leaf@12");
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
