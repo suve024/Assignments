ac1830748de3a2e8e6987b68b893b2db969d08da
Suvetha S <81619407+suve024@users.noreply.github.com>
2021-10-16 08:29:08 +0530

    Add files via upload

---
+++ week2/day2/EditExercise.java
@@ -0,0 +1,48 @@
+package week2.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EditExercise {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leafground.com/pages/Edit.html");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		// Enter your email address
+		driver.findElement(By.id("email")).sendKeys("swetha@gmail.com");
+
+		// Append a text and press keyboard tab
+		String attribute1 = driver.findElement(By.xpath("//*[@id='contentblock']/section/div[2]/div/div/input"))
+				.getAttribute("value");
+		String concat = attribute1.concat("Testleaf");
+		driver.findElement(By.xpath("//*[@id='contentblock']/section/div[2]/div/div/input")).clear();
+		driver.findElement(By.xpath("//*[@id='contentblock']/section/div[2]/div/div/input"))
+				.sendKeys(concat + Keys.TAB);
+
+		// Get default text entered
+		String attribute = driver.findElement(By.name("username")).getAttribute("value");
+		System.out.println("Default text entered is : " + attribute);
+
+		// Clear the text
+		driver.findElement(By.xpath("(//input[@name='username'])[2]")).clear();
+
+		// Confirm that edit field is disabled
+		boolean enabled = driver.findElement(By.xpath("//input[@disabled='true']")).isEnabled();
+		if (!enabled) {
+			System.out.println("Edit field is disabled");
+		} else {
+			System.out.println("Field is editable");
+		}
+	}
+
+}
